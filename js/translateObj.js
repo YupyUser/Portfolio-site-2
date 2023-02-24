552c5931e7a2549243263a7860df9cd491141653
YupiUser <Miragex69xWindow@gmail.com>
2023-02-24 17:33:59 +0300

    made header information, added backgr image

--- js/translateObj.js
+++ js/translateObj.js
@@ -27,6 +27,22 @@ let obj = {
         ru:'Получить тэг',
         en:'Get your tag'
     },
+    header__sub_title: {
+        ru:'Лучшие войс тэги для твоих битов',
+        en:'The best custom voice tags online'
+    },
+    header__additional_text: {
+        ru:`Войс тэг - это незаменимая вещь для каждого битмейкера. И наша 
+        команда готова изготовить его именно для тебя. Выбирай
+         любой из наших голосов, и выделяйся!`,
+        en:`Voice Tag is a necessity for every music producer. We offer a wide
+        range of voices and professional service. Brand your sound with us!
+        Become known`
+    },
+    header__button: {
+        ru:'Получить войс тэг',
+        en:'Get your custom tag'
+    }
 };
 
 export default obj;
