11d3268557c614b116d14ebc99991a0580c83513
YupiUser <Miragex69xWindow@gmail.com>
2023-03-03 18:38:22 +0300

    wrote js translate

--- js/translateObj.js
+++ js/translateObj.js
@@ -65,7 +65,70 @@ let obj = {
     how_it_works__warning: {
         ru:`Не беспокойтесь о произношении вашего имени. Мы свяжемся с вами, чтобы уточнить это.`,
         en:`Don't worry about the pronunciation of your name. We will contact you to clarify it.`
-    }
+    },
+
+
+    about_us__main_title: {
+        ru:`О нас`,
+        en:`About us`
+    },
+
+    about_us__elem_title_1: {
+        ru: `Не придётся ждать долго`,
+        en:`Fast Delivery`
+    },
+    about_us__elem_title_2: {
+        ru:`Ваши данные защищены`,
+        en:`Safety Payment`
+    },
+    about_us__elem_title_3: {
+        ru:`Больше тегов - больше скидка`,
+        en:`Package Deals`
+    },
+    about_us__elem_title_4: {
+        ru:`Широкий выбор голосов`,
+        en:`4 Voice Actor Options`
+    },
+    about_us__elem_title_5: {
+        ru:`Сделаем крутые эффекты`,
+        en:`Wet + Dry Recordings`
+    },
+    about_us__elem_title_6: {
+        ru:`Переделаем тэг без доплаты`,
+        en:`2 Free Revisions`
+    },
+
+    about_us__elem_adtnl_1:{
+        ru:`Войс тэг будет выслан на ваш email в течение 96 часов после оплаты`,
+        en:`.WAV files will be sent 96 hours after your payment is
+        processed. The download link with your voice tag order will be
+        sent to your email`
+    },
+    about_us__elem_adtnl_2:{
+        ru:`Безопасная оплата, а также широкий выбор способов оплаты`,
+        en:`Pay via PayPal or with your credit or debit card. We also have
+        some additional payment options in the order form`
+    },
+    about_us__elem_adtnl_3:{
+        ru:`Чем больше тэгов заказываешь, тем дешевле каждый из них обходится. Всё просто!`,
+        en:` Need multiple voice tags? We got special bulk pricing options.
+        Check the`
+    },
+    about_us__elem_adtnl_4:{
+        ru:`Мы предлагаем 4 голоса для англоязычных войс тэгов и 2 – для тэгов на русском`,
+        en:`Choose from female, male, kid or anime voice. Or get multiple
+        tags with different voices with package deals`
+    },
+    about_us__elem_adtnl_5:{
+        ru:`Наш звукоинженер реализует все твои пожелания. Только не забудь их указать!`,
+        en:`Choose from female, male, kid or anime voice. Or get multiple
+        tags with different voices with package deals`
+    },
+    about_us__elem_adtnl_6:{
+        ru:`Что-то пошло не так? Говори - перезапишем, обработаем заново. Две бесплатные правки включены`,
+        en:`Not the right sound for your brand? If you aren't happy with
+        your voice tag, we'll re-record it or change the effects!`
+    },
 };
 
 export default obj;
